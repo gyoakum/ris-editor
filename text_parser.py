907be3940acc4ed908c0d8faf49a084559795392
Graham Yoakum <gyoakum@gmail.com>
2020-03-28 20:56:29 -0400

    Catch duplicate record ids
    Added a check to catch if the record id has already been used.

--- text_parser.py
+++ text_parser.py
@@ -10,7 +10,7 @@ MAX_LINE_LENGTH = 40
 def check_line(line):
     '''Validate the line in the help file'''
     if line.startswith(';='):
-        match = re.search(r'^;=\s*\w{1,6}\s*$', line)
+        match = re.search(r'^;=\s*(\w{1,6})\s*$', line)
         return match.group(1) if match else None
     elif line.startswith('%'):
         match = re.search(r'^%(\d{2}):(\d)$', line)
@@ -35,20 +35,31 @@ def parse_helpfile(help_file):
     '''Read a plaintext help file and create a list of records'''
     records = []
     current_record = None
+    # Track which id codes have already been used
+    code_set = set()
     # Put inside a loop
     for line_number, line in enumerate(help_file):
         # print('{:0>3} {}'.format(line_number, line.rstrip()))
         if line.startswith(';='):
-            code = line[2:].strip()
+            match = check_line(line.rstrip())
+            if not match:
+                raise ParseError('Invalid record code',
+                        line=line,
+                        line_number=line_number)
             # Enforce maximum code size
-            if len(code.encode()) > 6:
+            if len(match.encode()) > 6:
                 raise ParseError('Record code exceeds maximum length of 6 bytes',
+                        line=line,
+                        line_number=line_number)
+            if match in code_set:
+                raise ParseError('Duplicate record code',
                         line=line,
                         line_number=line_number)
             # start new record
             if current_record:
                 records.append(current_record)
-            current_record = HelpRecord(code)
+            current_record = HelpRecord(match)
+            code_set.add(match)
         elif line.startswith('%'):
             match = check_line(line.rstrip())
             if match:
